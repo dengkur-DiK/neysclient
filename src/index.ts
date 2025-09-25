2686ec46de4663a8177b0b3c3b2d7390d00f2b03
Deng Kur <149548821+dengkur-DiK@users.noreply.github.com>
2025-09-25 22:28:17 +0000

    some changes

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+ import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // optional, if you have styles
+
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement as HTMLElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
